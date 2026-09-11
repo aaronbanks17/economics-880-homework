df7c243226d245c71e5a7c48db9cc2797992808b
Aaron Banks <aarontbanks17@gmail.com>
2026-09-10 19:49:36 -0500

    finished pset - ready to review

--- homework-1/parallel_script.jl
+++ homework-1/parallel_script.jl
@@ -1,27 +1,29 @@
 using Distributed
 addprocs(2)
+
 @everywhere using Parameters, Plots, SharedArrays #import the libraries we want
 include("parallel_functions.jl") #import the functions that solve our growth model
 @everywhere prim, res = Initialize() #initialize primitive and results structs
 @time Solve_model(prim, res) #solve the model!
 
 @unpack val_func, pol_func = res
-@unpack k_grid = prim
+@unpack k_grid, z_grid = prim
 
 ##############Make plots
 mkpath("output")
+
 #value function
-plot(k_grid, val_func, title="Value Function V(K)",ylabel = "value V(K)", label = "",xlabel = "capital K")
+plot(k_grid, val_func, title="Value Function V(K,Z)",ylabel = "value V(K,Z)", label = permutedims(["Z = $(z)" for z in z_grid]),xlabel = "capital K")
 savefig("output/Value_Functions_parallel.png")
 
 #policy functions
-plot(k_grid, pol_func, title="Policy Function K'(K)",ylabel = "policy K'(K)", label = "policy K'(K)",xlabel = "capital K",color="blue",linestyle=:solid)
+plot(k_grid, pol_func, title="Policy Function K'(K,Z)",ylabel = "policy K'(K,Z)", label = permutedims(["Z = $(z)" for z in z_grid]),xlabel = "capital K",linestyle=:solid)
 plot!(k_grid,k_grid,label = "45 degree",color="red",linestyle=:dash)
 savefig("output/Policy_Functions_parallel.png")
 
 #changes in policy function
 pol_func_δ = pol_func.-k_grid
-plot(k_grid, pol_func_δ, title="Saving Policy Function K'(K) - K",ylabel = "saving policy K'(K) - K", label = "",xlabel = "capital K")
+plot(k_grid, pol_func_δ, title="Saving Policy Function K'(K,Z) - K",ylabel = "saving policy K'(K,Z) - K", label = permutedims(["Z = $(z)" for z in z_grid]),xlabel = "capital K")
 hline!([0], linestyle=:dash, color=:black, label="")
 savefig("output/Policy_Functions_Changes_parallel.png")
 
